085b3a94e7688f135fbc868bc0e1795192e66338
John Rayes <live627@gmail.com>
2026-07-25 02:56:55 -0700

    Update SimplifiedIfReturnFixer.php

--- src/Fixer/ControlStructure/SimplifiedIfReturnFixer.php
+++ src/Fixer/ControlStructure/SimplifiedIfReturnFixer.php
@@ -102,8 +102,12 @@ final class SimplifiedIfReturnFixer extends AbstractFixer
     }
 
     /**
-     * @return array{isNegative: bool, indices: list<int>}|null
-     */
+     * @return null|array{
+     *     isNegative: bool,
+     *     start: int<0, max>,
+     *     end: int<0, max>
+     * }
+    */
     private function matchReturnSequence(Tokens $tokens, int $start): ?array
     {
         $count = $tokens->count();
@@ -130,7 +134,7 @@ final class SimplifiedIfReturnFixer extends AbstractFixer
 
             $prev = $tokens->getPrevMeaningfulToken($return);
             if (null !== $prev && $tokens[$prev]->equals('{')) {
-                $indices[] = $prev;
+                $start = $prev;
             }
 
             $next = $tokens->getNextMeaningfulToken($semi1);
@@ -139,8 +143,6 @@ final class SimplifiedIfReturnFixer extends AbstractFixer
             }
 
             if ($tokens[$next]->equals('}')) {
-                $indices[] = $next;
-
                 $next = $tokens->getNextMeaningfulToken($next);
                 if (null === $next) {
                     continue;
@@ -169,7 +171,7 @@ final class SimplifiedIfReturnFixer extends AbstractFixer
 
             return [
                 'isNegative' => 'false' === $value1,
-                'start' => $prev ?? $return,
+                'start' => $start ?? $return,
                 'end' => $semi2 - 1,
             ];
         }
