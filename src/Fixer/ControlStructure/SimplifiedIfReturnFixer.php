d97a295226cef71facd91ef356b8c0171120d821
John Rayes <live627@gmail.com>
2026-07-25 03:25:49 -0700

    Update SimplifiedIfReturnFixer.php

--- src/Fixer/ControlStructure/SimplifiedIfReturnFixer.php
+++ src/Fixer/ControlStructure/SimplifiedIfReturnFixer.php
@@ -107,7 +107,7 @@ final class SimplifiedIfReturnFixer extends AbstractFixer
     }
 
     /**
-     * @return array{isNegative: bool, indices: list<int>}|null
+     * @return null|array{isNegative: bool, indices: list<int<0, max>>}
      */
     private function matchReturnSequence(Tokens $tokens, int $start): ?array
     {
