5abea3c5a1aed7c0be4a314f9894a4e41235227b
John Rayes <live627@gmail.com>
2026-07-31 19:40:30 -0700

    typo

--- src/Tokenizer/AbstractTransformer.php
+++ src/Tokenizer/AbstractTransformer.php
@@ -26,7 +26,7 @@ use PhpCsFixer\Utils;
 abstract class AbstractTransformer implements TransformerInterface
 {
     /**
-     * @ver array<int, list<Token>|Token|Tokens>
+     * @var array<int, list<Token>|Token|Tokens>
      */
     protected array $slices = [];
 
